4c8362be8c6e1a9d47a85a2aacb6ce2bf44d6f4f
livisupajares <livisupajares@gmail.com>
2024-07-11 14:05:49 -0500

    Import diptera database

--- scripts/filter-high-confidence-annotation.R
+++ scripts/filter-high-confidence-annotation.R
@@ -1,3 +1,7 @@
 # ~~~~~ FILTER DIPTERA.CSV ~~~~~~
 # ===== Load libraries =====
 library("tidyverse")
+# ===== Importing diptera.csv =====
+diptera <- read.table("databases/diptera.csv",
+                      header = TRUE,
+                      sep = ",")
