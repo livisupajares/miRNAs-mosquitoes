c9994c82b691804207a00ab3933cd2a22c4d6c47
livisupajares <livisupajares@gmail.com>
2024-07-12 12:30:35 -0500

    Refractoring: convert into a function

--- scripts/delete-columns-NA.R
+++ scripts/delete-columns-NA.R
@@ -5,18 +5,9 @@ library("tidyverse")
 diptera <- read.table("databases/diptera.csv",
                       header = TRUE,
                       sep = ",")
-# ===== Encontrar columnas con valores NA =====
-# Contar valores NA en cada columna
-na_counts <- colSums(is.na(diptera))
 
-# Identificar columnas con NA valores
-all_na_columns <- which(na_counts == nrow(diptera))
 
-# Obtener el nombre de las columnas
-all_na_column_names <- names(all_na_columns)
-print(all_na_column_names)
 
-# ===== Borrar columna =====
 # Borrar columna "ref7_link" del dataframe diptera
 diptera$ref7_link <- NULL
 
