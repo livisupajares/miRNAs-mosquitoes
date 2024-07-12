2db61ea8238cfc26f3a455f124693b3bd498eaf6
livisupajares <livisupajares@gmail.com>
2024-07-11 20:03:20 -0500

    Save filtered databases to csv

--- scripts/filter-high-confidence-annotation.R
+++ scripts/filter-high-confidence-annotation.R
@@ -9,6 +9,15 @@ diptera <- read.table("databases/diptera.csv",
 # Filter diptera by conf high
 conf_high <- filter(diptera,
                     conf == "High")
+## Save conf_high to .csv
+write.csv(conf_high,
+          "databases/diptera-conf-high.csv",
+          row.names = FALSE)
+
 # Filter diptera by conf not enough data
 conf_low <- filter(diptera,
                     conf == "Not enough data")
+## Save conf_low to .csv
+write.csv(conf_low,
+          "databases/diptera-conf-low.csv",
+          row.names = FALSE)
