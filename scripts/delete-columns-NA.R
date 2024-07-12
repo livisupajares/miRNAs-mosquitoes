741c27ddcb2b7134c77f7b26872a0e8c18318d9d
livisupajares <livisupajares@gmail.com>
2024-07-12 11:59:26 -0500

    Identify which columns have all rows with NA

--- scripts/delete-columns-NA.R
+++ scripts/delete-columns-NA.R
@@ -5,3 +5,14 @@ library("tidyverse")
 diptera <- read.table("databases/diptera.csv",
                       header = TRUE,
                       sep = ",")
+# ===== Encontrar columnas con valores NA =====
+# Contar valores NA en cada columna
+na_counts <- colSums(is.na(diptera))
+
+# Identificar columnas con NA valores
+all_na_columns <- which(na_counts == nrow(diptera))
+
+# Obtener el nombre de las columnas
+all_na_column_names <- names(all_na_columns)
+print(all_na_column_names)
+
