af300ec97cbd93eb80bb336659610cab83512f0e
livisupajares <livisupajares@gmail.com>
2025-01-25 10:36:31 -0500

    Delete extra columns

--- scripts/test-cleaning-data-aae.R
+++ scripts/test-cleaning-data-aae.R
@@ -8,3 +8,10 @@ library("tidyverse")
 aae_mirna <- read.csv("databases/test/aae-mirna-seq.csv",
                       na.strings = c("","NA"))
 
+# ===== Deleting extra columns =====
+# Show name of columns
+colnames(aae_mirna)
+# Isolating miRNA sequences
+# # Remove columns with index 1 and from 5 to 39
+aae_mirna_seq <- aae_mirna[-c(1,5:39)]
+
