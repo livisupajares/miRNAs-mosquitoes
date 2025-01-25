93f3af1ef45665a85c9c1c4c93d41e8c66eeaca8
livisupajares <livisupajares@gmail.com>
2025-01-25 10:33:42 -0500

    Import data

--- scripts/test-cleaning-data-aae.R
+++ scripts/test-cleaning-data-aae.R
@@ -3,3 +3,8 @@
 # ===== Load libraries & files ===== 
 library("tidyverse")
 
+# ===== Importing data ===== 
+# Add NA to all empty spaces
+aae_mirna <- read.csv("databases/test/aae-mirna-seq.csv",
+                      na.strings = c("","NA"))
+
