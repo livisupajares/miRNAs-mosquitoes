b51c75c958c645d79c4780c49c0aacfb08222263
livisupajares <livisupajares@gmail.com>
2025-01-27 10:14:01 -0500

    Fix title

--- scripts/test-cleaning-data-aae.R
+++ scripts/test-cleaning-data-aae.R
@@ -1,8 +1,7 @@
-# ~~~~~ TEST = ISOLATING AEDES AEGYPTI MIRNA SEQUENCES ~~~~~~
+# ~~~~~ TEST = ISOLATING AEDES AEGYPTI MIRNA SEQUENCES ~~~~~~
 #
 # ===== Load libraries & files ===== 
 library("tidyverse")
-
 # ===== Importing data ===== 
 # Add NA to all empty spaces
 aae_mirna <- read.csv("databases/test/aae-mirna-seq.csv",
