cd6595b3b1c2ef5ad5a19ad843e3aa8041df9eb0
livisupajares <livisupajares@gmail.com>
2025-01-27 11:00:32 -0500

    Change variable name

--- scripts/test-cleaning-data-aae.R
+++ scripts/test-cleaning-data-aae.R
@@ -13,21 +13,21 @@ aae_mirna <- read.csv("databases/test/aae-mirna-seq.csv",
 colnames(aae_mirna)
 # Isolating miRNA sequences
 # # Remove columns with index 1, 3 and from 5 to 39
-aae_mirna_seq <- aae_mirna[-c(1,3,5:39)]
+aae_mirna_mat <- aae_mirna[-c(1,3,5:39)]
 
 # ==== Deleting all data with NA strings ==== 
-aae_mirna_seq <- aae_mirna_seq[complete.cases(aae_mirna_seq), ]
+aae_mirna_mat <- aae_mirna_mat[complete.cases(aae_mirna_mat), ]
 
 # ==== Deleting duplicated rows based on miRNA_name ====
-aae_mirna_seq <- aae_mirna_seq[!duplicated(aae_mirna_seq$mirna_name), ]
+aae_mirna_mat <- aae_mirna_mat[!duplicated(aae_mirna_mat$mirna_mat), ]
 
 # ==== Delete the numbers from miRNA strings ====
 # Delete numbers
-aae_mirna_seq$mat_seq <- gsub("[0-9]", "", aae_mirna_seq$mat_seq)
+aae_mirna_seq$mat_mat <- gsub("[0-9]", "", aae_mirna_mat$mat_seq)
 # Delete hyphens
-aae_mirna_seq$mat_seq <- gsub("-", "", aae_mirna_seq$mat_seq)
+aae_mirna_seq$mat_mat <- gsub("-", "", aae_mirna_mat$mat_seq)
 # Delete blank spaces
-aae_mirna_seq$mat_seq <- gsub(" ", "", aae_mirna_seq$mat_seq)
+aae_mirna_seq$mat_mat <- gsub(" ", "", aae_mirna_mat$mat_seq)
 
 # ==== Convert df to fasta ====
-df_2_fasta(aae_mirna_seq, "databases/test/aae_test_mirna.fasta")
+df_2_fasta(aae_mirna_mat, "databases/test/aae_test_mirna.fasta")
