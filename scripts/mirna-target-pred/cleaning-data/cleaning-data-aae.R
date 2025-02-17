d87ed7431c021ad8af272887b2f50b09ef9f0007
livisupajares <livisupajares@gmail.com>
2025-02-17 13:52:17 -0500

    refactor(aae-test): comment out unused test

--- scripts/mirna-target-pred/cleaning-data/cleaning-data-aae.R
+++ scripts/mirna-target-pred/cleaning-data/cleaning-data-aae.R
@@ -9,3 +9,10 @@ source("scripts/functions.R")
 ## Test 
 # aae_mirna <- read.csv("sequences/test/aae-mirna-seq.csv",
 #                       na.strings = c("","NA"))
+# aae_mirna_mat <- aae_mirna[-c(1,3,5:39)]
+# aae_mirna_mat <- aae_mirna_mat[complete.cases(aae_mirna_mat), ]
+# aae_mirna_mat <- aae_mirna_mat[!duplicated(aae_mirna_mat$mirna_name), ]
+# aae_mirna_mat$mat_seq <- gsub("[0-9]", "", aae_mirna_mat$mat_seq)
+# aae_mirna_mat$mat_seq <- gsub("-", "", aae_mirna_mat$mat_seq)
+# aae_mirna_mat$mat_seq <- gsub(" ", "", aae_mirna_mat$mat_seq)
+# df_2_fasta(aae_mirna_mat, "sequences/test/aae_test_mat.fasta")
