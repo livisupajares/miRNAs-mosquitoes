21d22548a8146f2d3970988f3079b92c64323d7d
livisupajares <livisupajares@gmail.com>
2025-02-18 15:44:17 -0500

    feat: create a function to transcribe T to U in miRNAs

--- scripts/build-database/convert-nucleotides.py
+++ scripts/build-database/convert-nucleotides.py
@@ -5,6 +5,11 @@ import pandas as pd
 
 # Import database
 aal_miRNAs = pd.read_csv("/Users/skinofmyeden/Documents/01-livs/14-programming/git/miRNAs-mosquitoes/sequences/aal-complete/aal-mirna-seq.csv")
+
+# Make function to transcribe miRNA T nucleotide into U
+def transcribe_dna_to_rna(miRNA_seq):
+    return str(Seq(miRNA_seq).transcribe())
+
 my_seq = Seq('ACTGactgACTG')
 my_seq = my_seq.transcribe()
 print(my_seq)
