da9a85c9e29428824ddd5129cef5b0cf14006d0c
livisupajares <livisupajares@gmail.com>
2025-02-18 15:32:58 -0500

    refactor: Import aal miRNAs sequences database

--- scripts/build-database/convert-nucleotides.py
+++ scripts/build-database/convert-nucleotides.py
@@ -1,9 +1,10 @@
 # Program to convert all T nucleotides into U nucleotides
 # Import Biopython and Pandas
 from Bio.Seq import Seq
-import pandas
+import pandas as pd
 
 # Import database
+aal_miRNAs = pd.read_csv("/Users/skinofmyeden/Documents/01-livs/14-programming/git/miRNAs-mosquitoes/sequences/aal-complete/aal-mirna-seq.csv")
 my_seq = Seq('ACTGactgACTG')
 my_seq = my_seq.transcribe()
 print(my_seq)
