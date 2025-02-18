016bc58db1175d945e854ed7ac15cbd4a21513f4
livisupajares <livisupajares@gmail.com>
2025-02-18 15:16:59 -0500

    chore: add comment about importing database

--- scripts/build-database/convert-nucleotides.py
+++ scripts/build-database/convert-nucleotides.py
@@ -1,6 +1,8 @@
 # Program to convert all T nucleotides into U nucleotides
 # Import Biopython and Pandas
 from Bio.Seq import Seq
+
+# Import database
 my_seq = Seq('ACTGactgACTG')
 my_seq = my_seq.transcribe()
 print(my_seq)
