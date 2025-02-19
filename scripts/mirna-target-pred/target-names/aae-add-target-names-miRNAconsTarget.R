f4e802aabbe8daf3edc33ae8acf2b1228dd1cd77
livisupajares <livisupajares@gmail.com>
2025-02-19 10:53:39 -0500

    feat: import miRNAconsTarget results for aae as three dataframes
    import miRNA target prediction results for aae as `aae_miranda`,
    `aae_ts`, `aae_pita` dataframes for subsequent analysis of vector base
    transcript accession codes.
    
    this will help to know the names of the mRNA targets for posterior
    enrichment analysis.

--- scripts/mirna-target-pred/target-names/aae-add-target-names-miRNAconsTarget.R
+++ scripts/mirna-target-pred/target-names/aae-add-target-names-miRNAconsTarget.R
@@ -2,3 +2,12 @@
 # ===== Load libraries & files ===== 
 library("dplyr")
 source("scripts/functions.R")
+
+# ===== Importing data ===== #
+# Add NA to all empty spaces
+# Miranda
+aae_miranda <- read.delim("results/miRNAconsTarget/miRNAconsTarget_aae_all/miranda/miranda.csv")
+# ts
+aae_ts <- read.delim("results/miRNAconsTarget/miRNAconsTarget_aae_all/ts/ts.csv")
+# PITA
+aae_pita <- read.delim("results/miRNAconsTarget/miRNAconsTarget_aae_all/pita/pita.csv")
