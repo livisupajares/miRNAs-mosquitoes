76c33a8995b6f33e2604844cf0be04e058938a23
livisupajares <livisupajares@gmail.com>
2025-02-19 08:40:30 -0500

    feat: import aal database to isolate miRNA sequences

--- scripts/mirna-target-pred/cleaning-data/cleaning-data-aal.R
+++ scripts/mirna-target-pred/cleaning-data/cleaning-data-aal.R
@@ -3,3 +3,8 @@
 # ===== Load libraries & files ===== 
 # library("tidyverse")
 source("scripts/functions.R")
+
+# ===== Importing data ===== 
+# Add NA to all empty spaces
+aal_mirna <- read.csv("sequences/aal-complete/aal-mirna-seq-U.csv",
+                      na.strings = c("","NA"))
