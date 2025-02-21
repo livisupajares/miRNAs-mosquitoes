1d51a9723c44a340d1b30d15d61324c8b0408fd6
livisupajares <livisupajares@gmail.com>
2025-02-21 09:33:27 -0500

    refactor: delete unused vector base aae database
    - Delete the database from VectorBase containing transcripts of aae.
    - It won't be used anymore for retriving the transcript names and
    Uniprot IDs of miRNA predicted targets in aae, because we chose in
    miRNAconsTarget to compare it against 3'UTR of aal Foshan.
    - 3'UTR of aae was not available on miRNAconsTarget

--- scripts/mirna-target-pred/target-names/aae-add-target-names-miRNAconsTarget.R
+++ scripts/mirna-target-pred/target-names/aae-add-target-names-miRNAconsTarget.R
@@ -15,11 +15,14 @@ aae_miranda <- read.delim("results/miRNAconsTarget/miRNAconsTarget_aae_all/miran
 # ts
 aae_ts <- read.delim("results/miRNAconsTarget/miRNAconsTarget_aae_all/targetspy-aae/targetspy-aae.csv")
 
-# vectorbase aae transcripts Foshan strain
-aae_vectorbase <- read.csv("databases/vector-base-mosquitos/aae-transcript-names-vectorbase.csv")
+# vectorbase aal transcripts Foshan strain
+aal_vectorbase <- read.csv("databases/vector-base-mosquitos/aal-transcript-names-vectorbase.csv")
 
 # ==== FIX DATA ==== #
 # Eliminate all decimal parts without rounding
 aae_miranda$mRNA <- sub("\\..*", "", aae_miranda$mRNA)
 aae_ts$mRNA <- sub("\\..*", "", aae_ts$mRNA)
 aae_pita$mRNA <- sub("\\..*", "", aae_pita$mRNA)
+
+# Change variable name to match the other databases
+aae_vectorbase <- aae_vectorbase %>% rename(source_id = transcript_id)
