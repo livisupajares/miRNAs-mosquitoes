a533c8740e62b86caae624c7b45459d95acad7c0
livisupajares <livisupajares@gmail.com>
2025-02-22 09:38:21 -0500

    chore: source script to import miRNA dataframes for extraction of uniprot ids
    - Sourced `aae-add-target-names-consTarget-miranda.R` to load miRNA dataframes into a list.
    - Facilitates iteration and processing of miRNA dataframes for isolating uniprot ids of targets.

--- scripts/mirna-target-pred/target-names/aae-add-target-names/save-uniprot-ids-txt.R
+++ scripts/mirna-target-pred/target-names/aae-add-target-names/save-uniprot-ids-txt.R
@@ -1,4 +1,5 @@
 # ~~~~~ EXTRACT UNIPROT IDS FROM EACH MIRNA AND SAVE THEM TO TXT FILES ~~~~~
+library(dplyr)
 # ==== IMPORT DF ====
-# Load the miRNA csv data miRanda from the previous script: aae-add-target-names-miRNAconsTarget-miranda.R
-input_dir_mir <- "results/miRNAconsTarget/miRNAconsTarget_aae_controles_all/miranda/"
+# Load the miRNA dataframes inside of a list to iterate over them
+source("scripts/mirna-target-pred/target-names/aae-add-target-names/aae-add-target-names-consTarget-miranda.R")
