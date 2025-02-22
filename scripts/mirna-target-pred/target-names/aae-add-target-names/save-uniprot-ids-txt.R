272a75f5458225dc3f6cada5d725b151876b0ed0
livisupajares <livisupajares@gmail.com>
2025-02-22 10:05:50 -0500

    feat: enable extraction and storage of UniProt IDs for miRNAs
    - Extracted UniProt IDs from `candidates_miranda` and saved them to text files.
    - Facilitates integration with tools or pipelines requiring UniProt ID data such as pantherdb for enrichment analysis.

--- scripts/mirna-target-pred/target-names/aae-add-target-names/save-uniprot-ids-txt.R
+++ scripts/mirna-target-pred/target-names/aae-add-target-names/save-uniprot-ids-txt.R
@@ -2,4 +2,13 @@
 library(dplyr)
 # ==== IMPORT DF ====
 # Load the miRNA dataframes inside of a list to iterate over them
-source("scripts/mirna-target-pred/target-names/aae-add-target-names/aae-add-target-names-consTarget-miranda.R")
+# source("scripts/mirna-target-pred/target-names/aae-add-target-names/aae-add-target-names-consTarget-miranda.R")
+
+# ==== EXTRACT UNIPROT IDS ====
+# Extract Uniprot IDs from each miRNA and save them to txt files
+lapply(names(candidates_miranda), function(miRNA_name) {
+  # Extract Uniprot IDs from uniprot_id column
+  uniprot_ids <- candidates_miranda[[miRNA_name]]$uniprot_id
+  # Save to txt file
+  write.table(uniprot_ids, file = paste0("results/miRNAconsTarget/miRNAconsTarget_aae_all/miranda-aae/uniprot-ids-aae/uniprots-aae-miranda/", miRNA_name, "-upid-aae-miranda", ".txt"), row.names = FALSE, col.names = FALSE)
+})
