ae28224230464d64dd88e7b8549c411676b6784f
livisupajares <livisupajares@gmail.com>
2025-02-22 08:56:22 -0500

    chore: load dplyr library for extracting UniProt IDs
    - Loaded the `dplyr` library to facilitate data manipulation tasks.
    - Supports the workflow to extract UniProt IDs from microRNAs and save them to text files.

---
+++ scripts/mirna-target-pred/target-names/aae-add-target-names/save-uniprot-ids-txt.R
@@ -0,0 +1,4 @@
+# ~~~~~ EXTRACT UNIPROT IDS FROM EACH MIRNA AND SAVE THEM TO TXT FILES ~~~~~
+# ==== IMPORT DF ====
+# Load the miRNA csv data miRanda from the previous script: aae-add-target-names-miRNAconsTarget-miranda.R
+input_dir_mir <- "results/miRNAconsTarget/miRNAconsTarget_aae_controles_all/miranda/"
