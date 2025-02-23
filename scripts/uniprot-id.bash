1a615181313256be322458c2c6b5cc1c9465ec21
livisupajares <livisupajares@gmail.com>
2025-02-22 19:41:26 -0500

    chore: add new path variable for targetscan

--- scripts/uniprot-id.bash
+++ scripts/uniprot-id.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Define the root folder where your input files are located
-root_folder=" /Users/skinofmyeden/Documents/01-livs/14-programming/git/miRNAs-mosquitoes/results/panther/aae-miranda-pantherdb/uniprots-aae-miranda" # Replace with the actual path to your folder
+root_folder_miranda=" /Users/skinofmyeden/Documents/01-livs/14-programming/git/miRNAs-mosquitoes/results/panther/aae-miranda-pantherdb/uniprots-aae-miranda" # Replace with the actual path to your folder
+
+root_folder_ts="/Users/skinofmyeden/Documents/01-livs/14-programming/git/miRNAs-mosquitoes/results/panther/aae-targetspy-pantherdb/uniprots-aae-targetspy"
 
 # Loop through all .txt files in the root folder
 for input_file in "$root_folder"/*.txt; do
