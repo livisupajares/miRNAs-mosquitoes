ee715a7df901f5b162cd878cba6da8c8565a7068
livisupajares <livisupajares@gmail.com>
2025-02-24 14:45:01 -0500

    feat: implement function to fetch protein sequences from NCBI
    - Added `fetch_protein_sequences` to retrieve protein records using
    accession numbers.
    - Uses NCBI's Entrez API (`efetch`) to fetch GenBank records in text
    format.
    - Appends successfully fetched records to a list and logs errors for
    failed fetches.
    - Enables retrieval of protein sequences for downstream analysis or
    processing.

--- scripts/mirna-target-pred/aligment-aal-to-aae/fetch-uniprot-prot-seq.py
+++ scripts/mirna-target-pred/aligment-aal-to-aae/fetch-uniprot-prot-seq.py
@@ -6,3 +6,23 @@ import pandas as pd
 
 # Set your email address (required by NCBI)
 Entrez.email = "livisu.pajares.r@upch.pe"
+
+# 
+
+# Function to fetch protein sequences from UniProtKB given a list of accession numbers.
+def fetch_protein_sequences(accession_numbers):
+    protein_records = []
+    
+    for acc in accession_numbers:
+        try:
+            # Fetch the GenBank record from NCBI using the accession number
+            handle = Entrez.efetch(db="protein", id=acc, rettype="gb", retmode="text")
+            record = SeqIO.read(handle, "genbank")
+            handle.close()
+            
+            # Append the record to the list of protein records
+            protein_records.append(record)
+        except Exception as e:
+                print(f"Error fetching {acc}: {e}")
+    
+    return protein_records
