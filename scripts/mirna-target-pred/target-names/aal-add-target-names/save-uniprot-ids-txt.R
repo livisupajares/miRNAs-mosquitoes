d080c8c3341bf5dac532c9f23ea056f1388f9e51
livisupajares <livisupajares@gmail.com>
2025-03-03 15:16:11 -0500

    refactor: change variables and path names from aae to aal

--- scripts/mirna-target-pred/target-names/aal-add-target-names/save-uniprot-ids-txt.R
+++ scripts/mirna-target-pred/target-names/aal-add-target-names/save-uniprot-ids-txt.R
@@ -2,7 +2,7 @@
 library(dplyr)
 # ==== IMPORT DF ====
 # Load the miRNA dataframes inside of a list to iterate over them
-source("scripts/mirna-target-pred/target-names/aae-add-target-names/aae-add-target-names-consTarget-miranda.R")
+source("scripts/mirna-target-pred/target-names/aal-add-target-names/aal-add-target-names-consTarget-miranda.R")
 source("scripts/mirna-target-pred/target-names/aae-add-target-names/aae-add-target-names-consTarget-targetspy.R")
 
 # ==== EXTRACT UNIPROT IDS ====
@@ -12,14 +12,14 @@ lapply(names(candidates_miranda), function(miRNA_name) {
   # Extract Uniprot IDs from uniprot_id column
   uniprot_ids <- candidates_miranda[[miRNA_name]]$uniprot_id
   # Save to txt file
-  write.table(uniprot_ids, file = paste0("results/panther/aae-miranda-pantherdb/uniprots-aae-miranda/", miRNA_name, "-upid-aae-miranda", ".txt"), row.names = FALSE, col.names = FALSE)
+  write.table(uniprot_ids, file = paste0("results/uniprots-aal-miranda/", miRNA_name, "-upid-aal-miranda", ".txt"), row.names = FALSE, col.names = FALSE)
 })
 
 # TargetSpy
 # Extract Uniprot IDs from each miRNA and save them to txt files
-lapply(names(candidates_ts), function(miRNA_name) {
+lapply(names(candidates_targetspy), function(miRNA_name) {
   # Extract Uniprot IDs from uniprot_id column
-  uniprot_ids <- candidates_ts[[miRNA_name]]$uniprot_id
+  uniprot_ids <- candidates_targetspy[[miRNA_name]]$uniprot_id
   # Save to txt file
-  write.table(uniprot_ids, file = paste0("results/panther/aae-targetspy-pantherdb/uniprots-aae-targetspy/", miRNA_name, "-upid-aae-targetspy", ".txt"), row.names = FALSE, col.names = FALSE)
+  write.table(uniprot_ids, file = paste0("results/uniprots-aal-targetspy/", miRNA_name, "-upid-aal-targetspy", ".txt"), row.names = FALSE, col.names = FALSE)
 })
