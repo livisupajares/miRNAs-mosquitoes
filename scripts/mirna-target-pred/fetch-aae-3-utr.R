3ddfa0f355d0a1b1a12f8ac534ad8df3fa526c0c
livisupajares <livisupajares@gmail.com>
2025-03-04 18:31:34 -0500

    chore: load and install `biomaRt` and `Biostrings` library

---
+++ scripts/mirna-target-pred/fetch-aae-3-utr.R
@@ -0,0 +1,11 @@
+# ~~~~~~~FETCH 3'UTR SEQUENCES FROM AAE TRANSCRIPTOME (ENSEMBLR) ~~~~~~~
+# ==== Load required libraries ====
+# Instructions to install bioconductor packages
+# if (!requireNamespace("BiocManager", quietly = TRUE)) {
+#   install.packages("BiocManager")
+# }
+# BiocManager::install("biomaRt")
+# BiocManager::install("Biostrings")
+
+library(biomaRt)
+library(Biostrings)
