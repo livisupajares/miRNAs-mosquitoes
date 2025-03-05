d8ab9c5eda05439a3b17bd27a07d4a5c2ae42a35
livisupajares <livisupajares@gmail.com>
2025-03-04 19:10:30 -0500

    chore: connect to metazoa ensembl

--- scripts/mirna-target-pred/fetch-aae-3-utr.R
+++ scripts/mirna-target-pred/fetch-aae-3-utr.R
@@ -9,3 +9,18 @@
 
 library(biomaRt)
 library(Biostrings)
+
+# ==== Connect to Ensembl ====
+# List available Ensembl Metazoa databases
+listMarts(host = "https://metazoa.ensembl.org")
+
+# Connect to Ensembl Metazoa database
+ensembl_metazoa <- useMart(
+  biomart = "metazoa_mart",
+  host = "https://metazoa.ensembl.org"
+)
+
+# List available datasets
+datasets <- listDatasets(ensembl_metazoa)
+
+# Specify the dataset for Aedes aegypti
