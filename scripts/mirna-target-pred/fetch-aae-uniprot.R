0faadec83834dee5eddf6c18c0323946e1d0e800
livisupajares <livisupajares@gmail.com>
2025-03-20 11:20:27 -0500

    style: add title and instruction comment

---
+++ scripts/mirna-target-pred/fetch-aae-uniprot.R
@@ -0,0 +1,8 @@
+# ~~~~~~~FETCH UNIPROT CODES FROM AAE TRANSCRIPTOME (ENSEMBLR) ~~~~~~~
+# ==== Load required libraries ====
+# Instructions to install bioconductor packages
+# if (!requireNamespace("BiocManager", quietly = TRUE)) {
+#   install.packages("BiocManager")
+# }
+# BiocManager::install("biomaRt")
+# BiocManager::install("Biostrings")
