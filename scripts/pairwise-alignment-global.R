26ff0fcb3641d32d502752b08ac02f39f1d2dc78
livisupajares <livisupajares@gmail.com>
2025-04-25 10:21:42 -0500

    chore: read fasta files

--- scripts/pairwise-alignment-global.R
+++ scripts/pairwise-alignment-global.R
@@ -7,3 +7,7 @@ BiocManager::install("Biostrings")
 # Carga la librería
 library(Biostrings)
 
+# Lee los archivos fasta
+albopictus <- readRNAStringSet("sequences/aal-complete/aal_mat_denv.fasta")
+aegypti <- readRNAStringSet("sequences/aae-complete/aae_mat_denv.fasta")
+
