d63a1594f7c84b1b146bc80d9cee533a2af02f9b
livisupajares <livisupajares@gmail.com>
2025-04-27 12:44:26 -0500

    chore: fix variable names
    This makes them more readable to work with plots and dataframes

--- scripts/aae-venny-fold-enrichment-graph.R
+++ scripts/aae-venny-fold-enrichment-graph.R
@@ -11,3 +11,10 @@ library(ggfittext) # Package to adjust text into ggplot2 plots
 aae_venny_shinygo <- read.csv("results/enrichment/aae/aae-venny_upregulated_shinygo.csv", header = TRUE)
 aae_venny_stringdb <- read.csv("results/enrichment/aae/aae-venny_upregulated_stringdb.csv", header = TRUE)
 
+# ==== Fix varible names ====
+# Fix column names shinygo
+colnames(aae_venny_shinygo) <- c("fdr", "n_genes", "pathway_genes", "fold_enrichment", "pathway", "url", "genes", "dataset")
+
+# Fix column names stringdb
+colnames(aae_venny_stringdb) <- c("term_id", "term_description", "observed_gene_count", "background_gene_count", "strength", "signal", "fdr", "matching_proteins_in_your_network_IDs", "matching_proteins_in_your_network_labels", "dataset")
+
