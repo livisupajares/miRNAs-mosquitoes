9efd7b57528a54179e76d5725f3c80d7920735d9
livisupajares <livisupajares@gmail.com>
2025-04-27 12:43:42 -0500

    chore: source `functions-R` and important libraries

--- scripts/aae-venny-fold-enrichment-graph.R
+++ scripts/aae-venny-fold-enrichment-graph.R
@@ -1,2 +1,8 @@
 # ~~~~~~ FOLD ENRICHMENT ~~~~~
 # This script is made to create a dispersion graph of the fold enrichment (SHINYGO & STRINGDB) of Aedes aegypti up-regulated miRNAs with common targets calculated by VENNY.
+
+# Source utils functions and import libraries
+source("scripts/functions.R")
+library(ggplot2)
+library(ggfittext) # Package to adjust text into ggplot2 plots
+
