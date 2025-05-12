94d5dd11cf054e8f193dc449cccc293417cb1674
livisupajares <livisupajares@gmail.com>
2025-05-12 10:52:00 -0500

    style: fix grammar

--- scripts/mirna-target-pred/cleaning-data/rnahybrid-aal-mir-34-5p-filter-targets.R
+++ scripts/mirna-target-pred/cleaning-data/rnahybrid-aal-mir-34-5p-filter-targets.R
@@ -1,6 +1,6 @@
 # ~~~~~ RNAHybrid post-processing aal-miR-34-5p ~~~~~ 
-# This script to parse RNAhybrid output into a readable dataframe,
-# It also used to extract and count G:U mismatches in the seed and flanking regions and filter by:
+# This a script to parse RNAhybrid output into a readable dataframe,
+# Then, it extracts and counts G:U mismatches in the seed and flanking regions and filters by:
 # one G:U mismatch was permitted in the seed sequence
 # maximum of three G:U mismatches were allowed in the flanking sequence
 # Then it filters and sort by lowest energy values and lowest p value.
