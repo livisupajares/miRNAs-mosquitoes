e08123162cfb150f722928cc48ea99d2a9eb76fe
livisupajares <livisupajares@gmail.com>
2025-06-09 14:55:30 -0500

    style: add clarifying comments

--- scripts/03-mirna-target-pred/00-target-names/02-aal-add-target-names/02-save-uniprot-ids-txt.R
+++ scripts/03-mirna-target-pred/00-target-names/02-aal-add-target-names/02-save-uniprot-ids-txt.R
@@ -1,6 +1,7 @@
 # ~~~~~ EXTRACT UNIPROT IDS FROM EACH MIRNA AND SAVE THEM TO TXT FILES ~~~~~
 library(dplyr)
-# ==== IMPORT DF ====
+
+# ==== IMPORT DATA ====
 # Load the miRNA dataframes inside of a list to iterate over them
 # miRanda
 source("scripts/03-mirna-target-pred/00-target-names/02-aal-add-target-names/00-aal-add-target-names-consTarget-miranda.R")
@@ -15,6 +16,8 @@ miranda_aal_uniprot_filtered <- read_csv("results/00-target-prediction/00-miRNAc
 # TargetSpy
 targetspy_aal_uniprot_filtered <- read_csv("results/00-target-prediction/00-miRNAconsTarget/aal_up/targetspy-aal/targetspy-aal-uniprot-filtered.csv")
  
+# ==== EXTRACT UNIPROT IDS ====
+# miRanda per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
 lapply(names(candidates_miranda), function(miRNA_name) {
   # Extract Uniprot IDs from uniprot_id column
@@ -23,8 +26,10 @@ lapply(names(candidates_miranda), function(miRNA_name) {
   writeLines(uniprot_ids, con = paste0("results/01-enrichment/shinygo/input/aal-miranda-per-mirna-shinygo/", miRNA_name, "-uniprot-aal-miranda.txt"))
 })
 
-# TargetSpy
+# miRanda all
 writeLines(uniprot_ids_all_miranda, con = "results/01-enrichment/shinygo/input/uniprot-aal-miranda-all.txt")
+
+# TargetSpy per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
 lapply(names(candidates_targetspy), function(miRNA_name) {
   # Extract Uniprot IDs from uniprot_id column
@@ -32,4 +37,5 @@ lapply(names(candidates_targetspy), function(miRNA_name) {
   # Save to txt file
   writeLines(uniprot_ids, con = paste0("results/01-enrichment/shinygo/input/aal-ts-per-mirna-shinygo/", miRNA_name, "-uniprot-aal-ts.txt"))
 })
+# TargetSpy all
 writeLines(uniprot_ids_all_ts, con = "results/01-enrichment/shinygo/input/uniprot-aal-ts-all.txt")
