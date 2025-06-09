9223073e50235da49368e13f32b283cd08d49c17
livisupajares <livisupajares@gmail.com>
2025-06-09 13:14:20 -0500

    feat: get all uniprot ids of all up-regulated miRNAs in Aedes aegypti

--- scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/04-save-uniprot-ids-txt.R
+++ scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/04-save-uniprot-ids-txt.R
@@ -9,6 +9,13 @@ source("scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/02-
 # TargetSpy
 source("scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/03-aae-add-target-names-consTarget-targetspy.R")
 
+# Load all targets 
+# miRanda
+miranda_aae_uniprot_filtered <- read_csv("results/00-target-prediction/00-miRNAconsTarget/aae_up/miranda-aae/miranda-aae-uniprot-filtered.csv")
+
+# TargetSpy
+targetspy_aae_uniprot_filtered <- read_csv("results/00-target-prediction/00-miRNAconsTarget/aae_up/targetspy-aae/targetspy-aae-uniprot-filtered.csv")
+
 # ==== EXTRACT UNIPROT IDS ====
 # miRanda per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
@@ -19,6 +26,9 @@ lapply(names(candidates_miranda), function(miRNA_name) {
   writeLines(uniprot_ids, con = paste0("results/01-enrichment/shinygo/input/aae-miranda-per-mirna-shinygo/", miRNA_name, "-uniprot-aae-miranda.txt"))
 })
 
+# miRanda all
+writeLines(miranda_aae_uniprot_filtered$uniprot_id, con = "results/01-enrichment/shinygo/input/uniprot-aae-miranda-all.txt")
+ 
 # TargetSpy per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
 lapply(names(candidates_ts), function(miRNA_name) {
@@ -27,3 +37,6 @@ lapply(names(candidates_ts), function(miRNA_name) {
   # Save to txt file
   writeLines(uniprot_ids, con = paste0("results/01-enrichment/shinygo/input/aae-ts-per-mirna-shinygo/", miRNA_name, "-uniprot-aae-targetspy.txt"))
 })
+
+# TargetSpy all
+writeLines(miranda_aae_uniprot_filtered$uniprot_id, con = "results/01-enrichment/shinygo/input/uniprot-aae-ts-all.txt")
