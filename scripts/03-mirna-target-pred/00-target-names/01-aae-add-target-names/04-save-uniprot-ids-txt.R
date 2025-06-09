b832c2c457982758aba90094814b8ec500695252
livisupajares <livisupajares@gmail.com>
2025-06-09 12:29:01 -0500

    style: fix comment

--- scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/04-save-uniprot-ids-txt.R
+++ scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/04-save-uniprot-ids-txt.R
@@ -1,7 +1,7 @@
 # ~~~~~ EXTRACT UNIPROT IDS FROM EACH MIRNA AND SAVE THEM TO TXT FILES ~~~~~
 library(dplyr)
 
-# ==== IMPORT DF ====
+# ==== IMPORT DATA ====
 # Load the miRNA dataframes inside of a list to iterate over them
 # miRanda
 source("scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/02-aae-add-target-names-consTarget-miranda.R")
@@ -10,7 +10,7 @@ source("scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/02-
 source("scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/03-aae-add-target-names-consTarget-targetspy.R")
 
 # ==== EXTRACT UNIPROT IDS ====
-# miRanda
+# miRanda per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
 lapply(names(candidates_miranda), function(miRNA_name) {
   # Extract Uniprot IDs from uniprot_id column
@@ -19,7 +19,7 @@ lapply(names(candidates_miranda), function(miRNA_name) {
   write.table(uniprot_ids, file = paste0("results/shinygo/aae-miranda-shinygo/", miRNA_name, "-uniprot-aae-miranda", ".txt"), row.names = FALSE, col.names = FALSE)
 })
 
-# TargetSpy
+# TargetSpy per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
 lapply(names(candidates_ts), function(miRNA_name) {
   # Extract Uniprot IDs from uniprot_id column
