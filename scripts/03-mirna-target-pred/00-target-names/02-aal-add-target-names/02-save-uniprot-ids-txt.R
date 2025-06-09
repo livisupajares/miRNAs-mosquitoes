9b10575f27d0d3a0409c4d9a4befb4c91b93a861
livisupajares <livisupajares@gmail.com>
2025-06-09 14:56:42 -0500

    feat: add function to uniprot ids in separated strings and flatten them
    This is used so one uniprot id is only one line

--- scripts/03-mirna-target-pred/00-target-names/02-aal-add-target-names/02-save-uniprot-ids-txt.R
+++ scripts/03-mirna-target-pred/00-target-names/02-aal-add-target-names/02-save-uniprot-ids-txt.R
@@ -20,22 +20,35 @@ targetspy_aal_uniprot_filtered <- read_csv("results/00-target-prediction/00-miRN
 # miRanda per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
 lapply(names(candidates_miranda), function(miRNA_name) {
-  # Extract Uniprot IDs from uniprot_id column
-  uniprot_ids <- candidates_miranda[[miRNA_name]]$uniprot_id
+  # Extract Uniprot IDs and split by comma if needed
+  uniprot_ids <- unlist(strsplit(as.character(candidates_miranda[[miRNA_name]]$uniprot_id), ","))
+  
+  # Remove empty strings if any
+  uniprot_ids <- Filter(nzchar, uniprot_ids)
+  
   # Save to txt file
   writeLines(uniprot_ids, con = paste0("results/01-enrichment/shinygo/input/aal-miranda-per-mirna-shinygo/", miRNA_name, "-uniprot-aal-miranda.txt"))
 })
 
 # miRanda all
+uniprot_ids_all_miranda <- unlist(strsplit(as.character(miranda_aal_uniprot_filtered$uniprot_id), ","))
+uniprot_ids_all_miranda <- Filter(nzchar, uniprot_ids_all_miranda)
 writeLines(uniprot_ids_all_miranda, con = "results/01-enrichment/shinygo/input/uniprot-aal-miranda-all.txt")
 
 # TargetSpy per miRNA
 # Extract Uniprot IDs from each miRNA and save them to txt files
 lapply(names(candidates_targetspy), function(miRNA_name) {
-  # Extract Uniprot IDs from uniprot_id column
-  uniprot_ids <- candidates_targetspy[[miRNA_name]]$uniprot_id
+  # Extract Uniprot IDs and split by comma if needed
+  uniprot_ids <- unlist(strsplit(as.character(candidates_targetspy[[miRNA_name]]$uniprot_id), ","))
+  
+  # Remove empty strings if any
+  uniprot_ids <- Filter(nzchar, uniprot_ids)
+  
   # Save to txt file
   writeLines(uniprot_ids, con = paste0("results/01-enrichment/shinygo/input/aal-ts-per-mirna-shinygo/", miRNA_name, "-uniprot-aal-ts.txt"))
 })
+
 # TargetSpy all
+uniprot_ids_all_ts <- unlist(strsplit(as.character(targetspy_aal_uniprot_filtered$uniprot_id), ","))
+uniprot_ids_all_ts <- Filter(nzchar, uniprot_ids_all_ts)
 writeLines(uniprot_ids_all_ts, con = "results/01-enrichment/shinygo/input/uniprot-aal-ts-all.txt")
