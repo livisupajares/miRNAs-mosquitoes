407c599a90db9f8494847370df2d6f654bcd1704
livisupajares <livisupajares@gmail.com>
2025-06-09 12:11:18 -0500

    refactor: change path of scripts and results for better organization

--- scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/04-save-uniprot-ids-txt.R
+++ scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/04-save-uniprot-ids-txt.R
@@ -1,9 +1,13 @@
 # ~~~~~ EXTRACT UNIPROT IDS FROM EACH MIRNA AND SAVE THEM TO TXT FILES ~~~~~
 library(dplyr)
+
 # ==== IMPORT DF ====
 # Load the miRNA dataframes inside of a list to iterate over them
-source("scripts/mirna-target-pred/target-names/aae-add-target-names/aae-add-target-names-consTarget-miranda.R")
-source("scripts/mirna-target-pred/target-names/aae-add-target-names/aae-add-target-names-consTarget-targetspy.R")
+# miRanda
+source("scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/02-aae-add-target-names-consTarget-miranda.R")
+
+# TargetSpy
+source("scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/03-aae-add-target-names-consTarget-targetspy.R")
 
 # ==== EXTRACT UNIPROT IDS ====
 # miRanda
