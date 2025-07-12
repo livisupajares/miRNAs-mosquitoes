7b7ae6ef4e6489d60c63fc6f3b84ce210bfde3f4
livisupajares <livisupajares@gmail.com>
2025-07-12 13:44:01 -0500

    chore: add script to fuse enrichment results
    Introduces fuse-enrichment-results.R to combine enrichment results for various miRNA and database combinations, supporting both Aedes aegypti and Aedes albopictus.

---
+++ scripts/04-enrichment/02-fetch-prot-names-enriched-processes/fuse-enrichment-results.R
@@ -0,0 +1,3 @@
+# ~~~~~ FUSE ENRICHMENT RESULTS ~~~~~
+# This script is used to fuse the enrichment results so at the end, we will get
+# per-mirna stringdb, per-mirna shinygo, venny stringdb, venny shinygo, all stringdb, all shinygo, where each will have Aedes aegypti and Aedes albopictus.
