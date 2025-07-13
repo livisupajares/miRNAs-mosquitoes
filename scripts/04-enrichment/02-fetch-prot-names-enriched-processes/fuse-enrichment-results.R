b8ab991602d1f8bca217450f9a9c030e28884fd9
livisupajares <livisupajares@gmail.com>
2025-07-12 21:07:35 -0500

    chore: fuse enrichment result data frames
    Introduced new data frames by row-binding AAE and AAL results for per miRNA, Venny, and all datasets from ShinyGO and STRINGdb. This prepares combined datasets for downstream analysis.

--- scripts/04-enrichment/02-fetch-prot-names-enriched-processes/fuse-enrichment-results.R
+++ scripts/04-enrichment/02-fetch-prot-names-enriched-processes/fuse-enrichment-results.R
@@ -26,3 +26,17 @@ aal_all_shinygo <- read.csv("results/01-enrichment/prot-names-enriched/aal-all-s
 # All stringdb
 aae_all_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aae-all-stringdb-export.csv")
 aal_all_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aal-all-stringdb-export.csv")
+
+# ==== FUSE DATA ====
+# Fuse data frames by row binding
+# Per miRNA
+per_mirna_shinygo <- rbind(aae_per_mirna_shinygo, aal_per_mirna_shinygo)
+per_mirna_stringdb <- rbind(aae_per_mirna_stringdb, aal_per_mirna_stringdb)
+
+# Venny
+venny_shinygo <- rbind(aae_venny_shinygo, aal_venny_shinygo)
+venny_stringdb <- rbind(aae_venny_stringdb, aal_venny_stringdb)
+
+# All
+all_shinygo <- rbind(aae_all_shinygo, aal_all_shinygo)
+all_stringdb <- rbind(aae_all_stringdb, aal_all_stringdb)
