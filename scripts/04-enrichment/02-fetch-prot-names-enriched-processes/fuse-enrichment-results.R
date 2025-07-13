16be7e8dbe13439bfaa19ef8a822710de37d19a4
livisupajares <livisupajares@gmail.com>
2025-07-12 20:16:51 -0500

    chore: add import statements for enrichment result datasets
    Added read.csv calls to import per-miRNA, venny, and all enrichment result datasets for both Aedes aegypti and Aedes albopictus from ShinyGO and STRINGdb sources. This prepares the script for further data fusion operations.

--- scripts/04-enrichment/02-fetch-prot-names-enriched-processes/fuse-enrichment-results.R
+++ scripts/04-enrichment/02-fetch-prot-names-enriched-processes/fuse-enrichment-results.R
@@ -1,3 +1,28 @@
 # ~~~~~ FUSE ENRICHMENT RESULTS ~~~~~
 # This script is used to fuse the enrichment results so at the end, we will get
 # per-mirna stringdb, per-mirna shinygo, venny stringdb, venny shinygo, all stringdb, all shinygo, where each will have Aedes aegypti and Aedes albopictus.
+
+# ==== IMPORT DATABASES TO BE FUSED ====
+## Per miRNA
+# Per-mirna shinygo
+aae_per_mirna_shinygo <- read.csv("results/01-enrichment/prot-names-enriched/aae-per-mirna-shinygo-export.csv")
+aal_per_mirna_shinygo <- read.csv("results/01-enrichment/prot-names-enriched/aal-per-mirna-shinygo-export.csv")
+# Per mirna stringdb
+aae_per_mirna_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aae-per-mirna-stringdb-export.csv")
+aal_per_mirna_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aal-per-mirna-stringdb-export.csv")
+
+## Venny
+# Venny shinygo
+aae_venny_shinygo <- read.csv("results/01-enrichment/prot-names-enriched/aae-venny-shinygo-export.csv")
+aal_venny_shinygo <- read.csv("results/01-enrichment/prot-names-enriched/aal-venny-shinygo-export.csv")
+# Venny stringdb
+aae_venny_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aae-venny-stringdb-export.csv")
+aal_venny_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aal-venny-stringdb-export.csv")
+
+## All
+# All shinygo
+aae_all_shinygo <- read.csv("results/01-enrichment/prot-names-enriched/aae-all-shinygo-export.csv")
+aal_all_shinygo <- read.csv("results/01-enrichment/prot-names-enriched/aal-all-shinygo-export.csv")
+# All stringdb
+aae_all_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aae-all-stringdb-export.csv")
+aal_all_stringdb <- read.csv("results/01-enrichment/prot-names-enriched/aal-all-stringdb-export.csv")
