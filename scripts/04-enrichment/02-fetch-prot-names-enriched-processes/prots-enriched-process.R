60f74a46690a586d0f95886af9fbb2751ba08d52
livisupajares <livisupajares@gmail.com>
2025-07-15 07:38:47 -0500

    feat: add script for processing enriched process proteins
    Introduces prots-enriched-process.R to split genes and pathway_genes into protein ID lists for each enriched process, save them with process names, and match protein IDs to names/descriptions using external mapping data.

---
+++ scripts/04-enrichment/02-fetch-prot-names-enriched-processes/prots-enriched-process.R
@@ -0,0 +1,5 @@
+# ~~~~~ PROTEINS OF ENRICHED PROCESES ~~~~~~
+# This script splits the genes and pathway_genes into a list of protein ids for each enriched process.
+# Then each list is saved into a dataframe with the process name and the list of protein ids.
+# Then, the list of protein ids is matched to their names and/or descriptions with imported dataframes of mapped protein ids from ensembl metazoa or stringdb.
+
