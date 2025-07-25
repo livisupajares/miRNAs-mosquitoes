f7bfa392e166b8dee965fe6ce16e9e3e9cfb2e62
livisupajares <livisupajares@gmail.com>
2025-07-25 12:42:46 -0500

    feat(enrichment): add grouping logic for Aal miRNA UniProt IDs to facilitate Venny analysis
    Added code to `01-aal-save-uniprot-ids-txt.R` to group the 24 Aal miRNA
    names into 4 sets of 6 for input into the Venny web tool. This includes:
    - Sorting miRNA names using `gtools::mixedsort` for natural order (e.g.,
    miR-2a before miR-10)
    - Splitting the sorted list into 4 groups
    - Printing the groups to console for manual copy/paste into Venny
    
    Also added `library(gtools)` dependency. This package helped sorting
    complex strings with letters and numbers such as `aal-miR-2a-5p` with
    the miRNA with the smallest number at the start of the list to the
    biggest number at the end of the list.

--- scripts/04-enrichment/00-save-uniprot-ids/01-aal-save-uniprot-ids-txt.R
+++ scripts/04-enrichment/00-save-uniprot-ids/01-aal-save-uniprot-ids-txt.R
@@ -1,6 +1,7 @@
 # ~~~~~ EXTRACT UNIPROT IDS FROM EACH MIRNA AND SAVE THEM TO TXT FILES ~~~~~
 library(dplyr)
 library(readr)
+library(gtools)
 
 # ==== IMPORT DATA ====
 # Load the miRNA dataframes inside of a list to iterate over them
@@ -29,3 +30,23 @@ lapply(names(candidates_miranda), function(miRNA_name) {
 uniprot_ids_all_miranda <- unlist(strsplit(as.character(miranda_aal_uniprot_filtered$uniprot_id), ","))
 uniprot_ids_all_miranda <- Filter(nzchar, uniprot_ids_all_miranda)
 writeLines(uniprot_ids_all_miranda, con = "results/02-enrichment/01-raw-input-output/shinygo/input/all/uniprot-aal-miranda-all.txt")
+
+# ===== DIVIDE ALL MIRNAS INTO 4 GROUPS ====
+# At this point we haven't done the Venny processing, therefore we should save
+# the list of aal miRNAs in a list, order them in ascending order and
+# divide them into 4 groups of 6 miRNAs each. Then print the result.
+# This will serve to copy and paste the uniprot IDs into venny's web server
+# input boxes (which are a maximun of 4) manually from the per-mirnas txt files
+
+# Extract the names of the miRNAs from the candidates_miranda list
+mirna_names <- names(candidates_miranda)
+print(mirna_names)
+
+# Sort the miRNA names in ascending order where aal-miR-2a-3p is first and
+# aal-miR-5706 is last
+mirna_names_sorted <- rev(mixedsort(mirna_names))
+print(mirna_names_sorted)
+
+# Divide the sorted miRNA names into 4 groups of 6 miRNAs each
+mirna_groups <- split(mirna_names_sorted, ceiling(seq_along(mirna_names_sorted) / 6))
+print(mirna_groups)
