b34fb55fad5b5bbd93fd97d0aa4932d8e95d0257
livisupajares <livisupajares@gmail.com>
2025-08-13 20:01:14 -0500

    chore: update paths

--- scripts/04-enrichment/01-fetch-sequences-stringdb/fetch-uniprot-prot-seq.py
+++ scripts/04-enrichment/01-fetch-sequences-stringdb/fetch-uniprot-prot-seq.py
@@ -13,12 +13,14 @@ import requests
 from tqdm import tqdm  # progress bars
 
 # Configuration
+# TODO make a root directory picker bc I'm too lazy to change root_dir
+# var
 # Add root directory
 root_dir = "/Users/skinofmyeden/Documents/01-livs/14-programming/git/miRNAs-mosquitoes"
 # Input directory to read .txt files with Uniprot kb accessions
-input_directory = f"{root_dir}/results/01-enrichment/shinygo/input/venny"
+input_directory = f"{root_dir}/results/02-enrichment/01-raw-input-output/shinygo/input/per-mirna/aal-miranda-per-mirna-shinygo"
 # Output directory to store the FASTA files
-output_dir = f"{root_dir}/sequences/04-enrichment/00-stringdb-input/venny"
+output_dir = f"{root_dir}/sequences/04-enrichment/00-stringdb-input/per-mirna/aal-miranda-per-mirna-stringdb"
 # Add a directory to deposit logs in case an accession can't be fetched
 log_directory = os.path.join(output_dir, "logs")
 # Base URL for UniProt REST API
