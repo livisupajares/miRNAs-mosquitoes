0068c1daa13ec2053e4bd73e75405b50dc49882c
livisupajares <livisupajares@gmail.com>
2025-09-18 19:03:03 -0500

    feat(ppi): add script to parse unmapped protein IDs from KEGG annotation
    Created new script `scripts/05-ppi/02-parse-unmapped-ids.R` to identify protein IDs that failed KEGG mapping. This script:
    - Imports annotated PPI data for both *Aedes aegypti* and *Aedes albopictus*
    - Uses `dplyr` and `tidylog` to filter for rows where `kegg_id` is NA
    - Extracts the `mapped_id` column for these unmapped entries
    - Prints the lists of unmapped IDs to the console
    
    This provides visibility into which proteins could not be annotated with KEGG identifiers, supporting further analysis or alternative annotation strategies.

---
+++ scripts/05-ppi/02-parse-unmapped-ids.R
@@ -0,0 +1,28 @@
+# ~~~~~~ SCRIPT FOR PARSING UNMAPPED PROTEIN IDs ~~~~~~
+# This script gets a list of the unmapped protein IDs from the annotated PPI data
+
+# ===== IMPORT LIBRARIES ======
+library("dplyr")
+# The goal of tidylog is to provide feedback about dplyr and tidyr operations. It provides simple wrapper functions for almost all dplyr and tidyr functions, such as filter, mutate, select, full_join, and group_by.
+# url: https://github.com/elbersb/tidylog
+library("tidylog", warn.conflicts = FALSE)
+
+# ===== IMPORT DATA ======
+# Import the annotated PPI data
+aae_ppi_annotated <- read.csv("results/03-ppi/aae_string_protein_annotations_with_kegg.tsv", sep = "\t", na.strings = c("", "NA"))
+aal_ppi_annotated <- read.csv("results/03-ppi/aal_string_protein_annotations_with_kegg.tsv", sep = "\t", na.strings = c("", "NA"))
+
+# ===== EXTRACT UNMAPPED IDs ======
+# Extract unmapped IDs from the annotated PPI data
+# Get mapped_ids from rows where kegg_is is NA
+aae_unmapped_ids <- aae_ppi_annotated %>%
+  filter(is.na(kegg_id)) %>%
+  select(mapped_id)
+
+aal_unmapped_ids <- aal_ppi_annotated %>%
+  filter(is.na(kegg_id)) %>%
+  select(mapped_id)
+
+# Print unmapped_ids to console without row ids
+print(aae_unmapped_ids, row.names = FALSE)
+print(aal_unmapped_ids, row.names = FALSE)
