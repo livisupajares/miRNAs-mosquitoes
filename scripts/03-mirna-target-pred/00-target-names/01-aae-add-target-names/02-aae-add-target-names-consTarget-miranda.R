fb165e8f27f15978c227457247150bd8a2dd8e08
livisupajares <livisupajares@gmail.com>
2025-09-27 20:33:06 -0500

    docs(target-prediction): update script description with DENV-2 context and energy threshold
    Updated the header comment in `02-aae-add-target-names-consTarget-miranda.R` to specify that the miRNA target predictions are for up-regulated Aedes aegypti miRNAs during DENV-2 infection and to reflect the updated energy threshold (-20 kcal/mol) used for filtering targets.

--- scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/02-aae-add-target-names-consTarget-miranda.R
+++ scripts/03-mirna-target-pred/00-target-names/01-aae-add-target-names/02-aae-add-target-names-consTarget-miranda.R
@@ -1,8 +1,9 @@
 # ~~~~~ ADD TARGET NAMES ~~~~~ #
 # This script adds target names to the Aedes aegypti miRNA target prediction
-# results from the miRanda algorithm.
+# results from the miRanda algorithm (targets from up-regulated miRNAs during
+# DENV-2)
 # This script also filters the results to find the best mRNA target candidates
-# according to a specific criteria (highest score, lowest energy (-14 kcal/mol
+# according to a specific criteria (highest score, lowest energy (-20 kcal/mol
 # cutoff)
 
 # NOTE: We were supposed to merge VectorBase info into predicted targets by
