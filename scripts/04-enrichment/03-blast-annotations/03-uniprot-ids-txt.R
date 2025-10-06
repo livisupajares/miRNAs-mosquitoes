4c6535654d945ed58ff4f98b94b01e4bdec3e977
livisupajares <livisupajares@gmail.com>
2025-10-06 13:03:19 -0500

    feat(enrichment): add script to extract unique UniProt IDs for BLAST annotation
    Created new script `03-uniprot-ids-txt.R` in the `scripts/04-enrichment/03-blast-annotations/` directory. This script:
    - Loads the species-separated UniProt-annotated STRINGdb enrichment results generated by `02-separate-df-by-species.R`
    - Extracts unique UniProt IDs from the `matching_proteins_id_network` column for each of the six dataframes
    - Writes these unique IDs to separate `.txt` files in `results/02-enrichment/05-blast-annotation/uniprot_ids_txt/`
    
    The generated text files will serve as input for the subsequent Python script (`fetch-uniprot-prot-seq.py`) to fetch protein sequences for targeted BLAST analysis.

---
+++ scripts/04-enrichment/03-blast-annotations/03-uniprot-ids-txt.R
@@ -0,0 +1,23 @@
+# ~~~~~~ CONVERT UNIQUE UNIPROT IDS TO TXT FILES ~~~~~~
+# This script converts unique `matching_proteins_id_network` into text files and then using the fetch-uniprot-prot-seq.py script to get the sequences for subsequent BLAST analysis.
+
+# ===== Add libraries =====
+library(dplyr)
+library(tidylog, warn.conflicts = FALSE)
+
+# ===== Import data =====
+source("scripts/04-enrichment/03-blast-annotations/02-separate-df-by-species.R")
+
+# ===== Extract Uniprot IDs ====
+# Only Unique IDs
+writeLines(unique(full_expanded_all_down_stringdb_annotated$matching_proteins_id_network), con = "results/02-enrichment/05-blast-annotation/uniprot_ids_txt/aae_all_down_annotated.txt")
+
+writeLines(unique(aae_all_annotated$matching_proteins_id_network), con = "results/02-enrichment/05-blast-annotation/uniprot_ids_txt/aae_all_annotated.txt")
+
+writeLines(unique(aal_all_annotated$matching_proteins_id_network), con = "results/02-enrichment/05-blast-annotation/uniprot_ids_txt/aal_all_annotated.txt")
+
+writeLines(unique(full_expanded_per_mirna_down_stringdb_annotated$matching_proteins_id_network), con = "results/02-enrichment/05-blast-annotation/uniprot_ids_txt/aae_per_mirna_down_annotated.txt")
+
+writeLines(unique(aae_per_mirna_annotated$matching_proteins_id_network), con = "results/02-enrichment/05-blast-annotation/uniprot_ids_txt/aae_per_mirna_annotated.txt")
+
+writeLines(unique(aal_per_mirna_annotated$matching_proteins_id_network), con = "results/02-enrichment/05-blast-annotation/uniprot_ids_txt/aal_per_mirna_annotated.txt")
