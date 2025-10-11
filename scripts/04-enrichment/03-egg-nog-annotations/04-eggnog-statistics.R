d7ab86e18f43ae2aa1bd1e1d7d1694f575e86594
livisupajares <livisupajares@gmail.com>
2025-10-11 14:37:08 -0500

    feat(enrichment): add libraries to EggNOG statistics script
    Enhanced `04-eggnog-statistics.R` by importing the necessary libraries (`dplyr` and `tidylog`) for data manipulation and logging. This prepares the script for analyzing the completeness of EggNOG Mapper annotations by counting annotated versus unannotated UniProt IDs.

--- scripts/04-enrichment/03-egg-nog-annotations/04-eggnog-statistics.R
+++ scripts/04-enrichment/03-egg-nog-annotations/04-eggnog-statistics.R
@@ -2,3 +2,7 @@
 # This script is to see how many uniprot ids were not annotated with eggnog-mapper
 # and how many were annotated with eggnog-mapper.
 
+# ===== LOAD LIBRARIES =====
+library(dplyr)
+library(tidylog, warn.conflicts = FALSE)
+
