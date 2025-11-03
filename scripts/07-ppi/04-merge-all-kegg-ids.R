6c1757f7ff9d2db0de5fd91ce6f9fddefa7a8b0d
livisupajares <livisupajares@gmail.com>
2025-11-03 14:14:47 -0500

    feat(ppi): add script to merge KEGG IDs from blastKOALA results
    Created new script `scripts/07-ppi/04-merge-all-kegg-ids.R` to handle the integration of KEGG IDs obtained from the blastKOALA analysis. This script will be used to merge the newly acquired KEGG annotations with the original protein annotation tables, facilitating the identification of UniProt IDs that still lack KEGG mappings after the blastKOALA supplementation.

---
+++ scripts/07-ppi/04-merge-all-kegg-ids.R
@@ -0,0 +1,5 @@
+# ~~~~ MERGE ALL KEGG IDS ~~~~~
+# After extracting the kegg ids from the blastKOALA
+# we need to merge these new kegg ids to the original
+# table and then see how many uniprot ids doesn't have 
+# kegg ids
