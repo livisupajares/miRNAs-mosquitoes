1f4b97e30f41f3683a4164e259cf9690946bbf59
livisupajares <livisupajares@gmail.com>
2025-11-18 20:23:05 -0500

    feat(orthoscape): add import for KEGG-UniProt mapping data
    Enhanced `05-orthoscape-import.R` by adding data import functionality for the KEGG-UniProt mapping files:
    - Imported the final KEGG-UniProt mapping results for *Aedes aegypti* (`aae_keggids.csv`) and *Aedes albopictus* (`aal_keggids.csv`) generated by the previous script (`04-merge-all-kegg-ids.R`)
    - Used `read.csv` with default settings to load the processed annotation files containing KEGG IDs, UniProt IDs, and protein names
    - Added comments to clarify the purpose of each imported dataset (KEGG-UniProt mappings and protein names)
    
    This prepares the necessary mapping data for the subsequent steps of integrating KEGG annotations into the STRING PPI network files.

--- scripts/08-taxonomic-comparison/05-orthoscape-import.R
+++ scripts/08-taxonomic-comparison/05-orthoscape-import.R
@@ -4,11 +4,11 @@
 # - After node2_string_id, add `node1_uniprot_id` and `node2_uniprot_id` with only uniprot ids (not the string taxon ID)
 # - `id1` and `id2`: kegg ids of node 1 and 2
 # `protein_name1` and `protein_name2`: eggnog mapper name.
-# 
+#
 # This script will take `aae_sring_node_degrees.tsv` and `aal_string_node_degrees.tsv` and add the following columns:
 # - Between `Identifier` and `node_degree`, add `id` (kegg ids) and `protein_name` (eggnog mapper)
 # Finally, it will remove nodes without kegg ids in both files for both species
-# 
+#
 
 # ==== IMPORT LIBRARIES ====
 library(dplyr)
@@ -24,3 +24,8 @@ aae_degree <- read.csv("results/03-taxonomic-comparison/02-orthoscape-import/aae
 aal_degree <- read.csv("results/03-taxonomic-comparison/02-orthoscape-import/aal/aal_string_node_degrees.tsv", sep = "\t")
 
 # Import data that have the kegg ids, uniprot ids and protein names (eggnog mapper)
+## kegg ids + uniprot ids
+aae_kegg_uniprot <- read.csv("results/03-taxonomic-comparison/01-add-kegg-ids/03-final_kegg_ids/aae_keggids.csv")
+aal_kegg_uniprot <- read.csv("results/03-taxonomic-comparison/01-add-kegg-ids/03-final_kegg_ids/aal_keggids.csv")
+
+## protein names
